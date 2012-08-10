4a400bcc8d18ff575d672f97fbbf4814403a55aa
Surion <realmsofwarcraft@gmail.com>
2012-08-10 13:38:52 -0500

    [0056] Fix for creature spawnmask 1 warning in map 0. Map 0 was removed from DBC
    Conflicts:
    	src/game/ObjectMgr.cpp
    
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/ObjectMgr.cpp
+++ src/game/ObjectMgr.cpp
@@ -1345,8 +1345,9 @@ void ObjectMgr::LoadCreatures()
             continue;
         }
 
-        if (data.spawnMask & ~spawnMasks[data.mapid])
-            sLog.outErrorDb("Table `creature` have creature (GUID: %u) that have wrong spawn mask %u including not supported difficulty modes for map (Id: %u).",guid, data.spawnMask, data.mapid );
+        // Map 0 was removed from dbc as of 4.x.x
+        if ((data.spawnMask & ~spawnMasks[data.mapid]) && data.mapid !=0)
+            sLog.outErrorDb("Table `creature` have creature (GUID: %u) that have wrong spawn mask %u including not supported difficulty modes for map (Id: %u).", guid, data.spawnMask, data.mapid);
 
         bool ok = true;
         for (uint32 diff = 0; diff < MAX_DIFFICULTY - 1 && ok; ++diff)
