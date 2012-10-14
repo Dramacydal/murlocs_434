mangos/MaNGOS

[12159] Related bg cleanups of EY cleanup

src/game/BattleGround/BattleGroundAB.h
@@ -227,10 +227,10 @@ class BattleGroundAB : public BattleGround
         BG_AB_BannerTimer   m_BannerTimers[BG_AB_NODES_MAX];
         uint32              m_NodeTimers[BG_AB_NODES_MAX];
         uint32              m_lastTick[BG_TEAMS_COUNT];
-        uint32              m_HonorScoreTics[BG_TEAMS_COUNT];
+        uint32              m_honorScoreTicks[BG_TEAMS_COUNT];
         uint32              m_ReputationScoreTics[BG_TEAMS_COUNT];
         bool                m_IsInformedNearVictory;
-        uint32              m_HonorTics;
+        uint32              m_honorTicks;
         uint32              m_ReputationTics;
         // need for achievements
         bool                m_TeamScores500Disadvantage[BG_TEAMS_COUNT];

src/game/BattleGround/BattleGroundWS.h
@@ -110,17 +110,21 @@ class BattleGroundWS : public BattleGround
         virtual void StartingEventOpenDoors() override;
 
         /* BG Flags */
-        ObjectGuid GetAllianceFlagPickerGuid() const { return m_FlagKeepers[BG_TEAM_ALLIANCE]; }
-        ObjectGuid GetHordeFlagPickerGuid() const   { return m_FlagKeepers[BG_TEAM_HORDE]; }
-        void SetAllianceFlagPicker(ObjectGuid guid) { m_FlagKeepers[BG_TEAM_ALLIANCE] = guid; }
-        void SetHordeFlagPicker(ObjectGuid guid)    { m_FlagKeepers[BG_TEAM_HORDE] = guid; }
-        void ClearAllianceFlagPicker()              { m_FlagKeepers[BG_TEAM_ALLIANCE].Clear(); }
-        void ClearHordeFlagPicker()                 { m_FlagKeepers[BG_TEAM_HORDE].Clear(); }
-        bool IsAllianceFlagPickedup() const         { return !m_FlagKeepers[BG_TEAM_ALLIANCE].IsEmpty(); }
-        bool IsHordeFlagPickedup() const            { return !m_FlagKeepers[BG_TEAM_HORDE].IsEmpty(); }
+        ObjectGuid GetAllianceFlagCarrierGuid() const { return m_flagCarrierAlliance; }
+        ObjectGuid GetHordeFlagCarrierGuid() const { return m_flagCarrierHorde; }
+
+        void SetAllianceFlagCarrier(ObjectGuid guid) { m_flagCarrierAlliance = guid; }
+        void SetHordeFlagCarrier(ObjectGuid guid) { m_flagCarrierHorde = guid; }
+
+        void ClearAllianceFlagCarrier() { m_flagCarrierAlliance.Clear(); }
+        void ClearHordeFlagCarrier() { m_flagCarrierHorde.Clear(); }
+
+        bool IsAllianceFlagPickedUp() const { return !m_flagCarrierAlliance.IsEmpty(); }
+        bool IsHordeFlagPickedUp() const { return !m_flagCarrierHorde.IsEmpty(); }
+
         void RespawnFlag(Team team, bool captured);
-        void RespawnFlagAfterDrop(Team team);
-        uint8 GetFlagState(Team team)             { return m_FlagState[GetTeamIndexByTeamId(team)]; }
+        void RespawnDroppedFlag(Team team);
+        uint8 GetFlagState(Team team) { return m_FlagState[GetTeamIndexByTeamId(team)]; }
 
         /* Battleground Events */
         virtual void EventPlayerDroppedFlag(Player* source) override;
@@ -144,13 +148,9 @@ class BattleGroundWS : public BattleGround
         ObjectGuid const& GetDroppedFlagGuid(Team team) const { return m_DroppedFlagGuid[GetTeamIndexByTeamId(team)];}
         virtual void FillInitialWorldStates(WorldPacket& data, uint32& count) override;
 
-        /* Scorekeeping */
-        uint32 GetTeamScore(Team team) const            { return m_TeamScores[GetTeamIndexByTeamId(team)]; }
-        void AddPoint(Team team, uint32 points = 1)     { m_TeamScores[GetTeamIndexByTeamId(team)] += points; }
-        void SetTeamPoint(Team team, uint32 points = 0) { m_TeamScores[GetTeamIndexByTeamId(team)] = points; }
-        void RemovePoint(Team team, uint32 points = 1)  { m_TeamScores[GetTeamIndexByTeamId(team)] -= points; }
     private:
-        ObjectGuid m_FlagKeepers[BG_TEAMS_COUNT];
+        ObjectGuid m_flagCarrierAlliance;
+        ObjectGuid m_flagCarrierHorde;
 
         ObjectGuid m_DroppedFlagGuid[BG_TEAMS_COUNT];
         uint8 m_FlagState[BG_TEAMS_COUNT];

Dramacydal/murlocs_434

Conflicts:
	src/game/BattleGround/BattleGroundAB.cpp
	src/game/BattleGround/BattleGroundAB.h
	src/game/BattleGround/BattleGroundWS.cpp
	src/game/BattleGround/BattleGroundWS.h
Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

src/game/BattleGround/BattleGroundAB.h
@@ -237,11 +237,11 @@ class BattleGroundAB : public BattleGround
         BG_AB_BannerTimer   m_BannerTimers[BG_AB_NODES_MAX];
         uint32              m_NodeTimers[BG_AB_NODES_MAX];
         uint32              m_lastTick[PVP_TEAM_COUNT];
-        uint32              m_HonorScoreTics[PVP_TEAM_COUNT];
+        uint32              m_honorScoreTicks[PVP_TEAM_COUNT];
         uint32              m_ReputationScoreTics[PVP_TEAM_COUNT];
         uint32              m_ExperiencesTicks[PVP_TEAM_COUNT];
         bool                m_IsInformedNearVictory;
-        uint32              m_HonorTics;
+        uint32              m_honorTicks;
         uint32              m_ReputationTics;
         // need for achievements
         bool                m_TeamScores500Disadvantage[PVP_TEAM_COUNT];

src/game/BattleGround/BattleGroundWS.h
@@ -120,17 +120,21 @@ class BattleGroundWS : public BattleGround
         virtual void StartingEventOpenDoors() override;
 
         /* BG Flags */
-        ObjectGuid GetAllianceFlagPickerGuid() const{ return m_FlagKeepers[TEAM_INDEX_ALLIANCE]; }
-        ObjectGuid GetHordeFlagPickerGuid() const   { return m_FlagKeepers[TEAM_INDEX_HORDE]; }
-        void SetAllianceFlagPicker(ObjectGuid guid) { m_FlagKeepers[TEAM_INDEX_ALLIANCE] = guid; }
-        void SetHordeFlagPicker(ObjectGuid guid)    { m_FlagKeepers[TEAM_INDEX_HORDE] = guid; }
-        void ClearAllianceFlagPicker()              { m_FlagKeepers[TEAM_INDEX_ALLIANCE].Clear(); }
-        void ClearHordeFlagPicker()                 { m_FlagKeepers[TEAM_INDEX_HORDE].Clear(); }
-        bool IsAllianceFlagPickedup() const         { return !m_FlagKeepers[TEAM_INDEX_ALLIANCE].IsEmpty(); }
-        bool IsHordeFlagPickedup() const            { return !m_FlagKeepers[TEAM_INDEX_HORDE].IsEmpty(); }
+        ObjectGuid GetAllianceFlagCarrierGuid() const { return m_flagCarrierAlliance; }
+        ObjectGuid GetHordeFlagCarrierGuid() const { return m_flagCarrierHorde; }
+
+        void SetAllianceFlagCarrier(ObjectGuid guid) { m_flagCarrierAlliance = guid; }
+        void SetHordeFlagCarrier(ObjectGuid guid) { m_flagCarrierHorde = guid; }
+
+        void ClearAllianceFlagCarrier() { m_flagCarrierAlliance.Clear(); }
+        void ClearHordeFlagCarrier() { m_flagCarrierHorde.Clear(); }
+
+        bool IsAllianceFlagPickedUp() const { return !m_flagCarrierAlliance.IsEmpty(); }
+        bool IsHordeFlagPickedUp() const { return !m_flagCarrierHorde.IsEmpty(); }
+
         void RespawnFlag(Team team, bool captured);
-        void RespawnFlagAfterDrop(Team team);
-        uint8 GetFlagState(Team team)             { return m_FlagState[GetTeamIndex(team)]; }
+        void RespawnDroppedFlag(Team team);
+        uint8 GetFlagState(Team team) { return m_FlagState[GetTeamIndex(team)]; }
 
         /* Battleground Events */
         virtual void EventPlayerDroppedFlag(Player *Source) override;
@@ -154,18 +158,11 @@ class BattleGroundWS : public BattleGround
         ObjectGuid const& GetDroppedFlagGuid(Team team) const { return m_DroppedFlagGuid[GetTeamIndex(team)];}
         virtual void FillInitialWorldStates(WorldPacket& data, uint32& count) override;
 
-        /* Scorekeeping */
-        uint32 GetTeamScore(Team team) const            { return m_TeamScores[GetTeamIndex(team)]; }
-        void AddPoint(Team team, uint32 Points = 1)     { m_TeamScores[GetTeamIndex(team)] += Points; }
-        void SetTeamPoint(Team team, uint32 Points = 0) { m_TeamScores[GetTeamIndex(team)] = Points; }
-        void RemovePoint(Team team, uint32 Points = 1)  { m_TeamScores[GetTeamIndex(team)] -= Points; }
-
         uint32 GetPlayerScore(Player *Source, uint32 type);
 
     private:
-        void CheckBuggers();
-
-        ObjectGuid m_FlagKeepers[PVP_TEAM_COUNT];
+        ObjectGuid m_flagCarrierAlliance;
+        ObjectGuid m_flagCarrierHorde;
 
         ObjectGuid m_DroppedFlagGuid[PVP_TEAM_COUNT];
         uint8 m_FlagState[PVP_TEAM_COUNT];