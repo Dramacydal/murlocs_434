mangostwo/server

Fixup last commit (why didn't it ammend..

src/game/Group.cpp
@@ -27,7 +27,7 @@
 #include "Group.h"
 #include "Formulas.h"
 #include "ObjectAccessor.h"
-#include "BattleGound/BattleGround.h"
+#include "BattleGround/BattleGround.h"
 #include "MapManager.h"
 #include "MapPersistentStateMgr.h"
 #include "Util.h"

Dramacydal/murlocs_434

Conflicts:
	src/game/AchievementMgr.cpp
	src/game/BattleGround/BattleGroundAB.cpp
	src/game/BattleGround/BattleGroundAV.cpp
	src/game/BattleGround/BattleGroundDS.cpp
	src/game/BattleGround/BattleGroundEY.cpp
	src/game/BattleGround/BattleGroundIC.cpp
	src/game/BattleGround/BattleGroundMgr.cpp
	src/game/BattleGround/BattleGroundRV.cpp
	src/game/BattleGround/BattleGroundSA.cpp
	src/game/BattleGround/BattleGroundWS.cpp
	src/game/Group.cpp
Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

src/game/Group.cpp
@@ -28,7 +28,7 @@
 #include "Formulas.h"
 #include "ObjectAccessor.h"
 #include "BattleGound/BattleGround.h"
-#include "BattleGroundMgr.h"
+#include "BattleGound/BattleGroundMgr.h"
 #include "OutdoorPvP/OutdoorPvP.h"
 #include "OutdoorPvP/OutdoorPvPWG.h"
 #include "MapManager.h"