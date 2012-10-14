mangos/MaNGOS

[12123] Move BattleGround code to separate BattleGround directory. Also some corrects to unload BG's

src/game/SpellEffects.cpp
@@ -42,10 +42,10 @@
 #include "Creature.h"
 #include "Totem.h"
 #include "CreatureAI.h"
-#include "BattleGroundMgr.h"
-#include "BattleGround.h"
-#include "BattleGroundEY.h"
-#include "BattleGroundWS.h"
+#include "BattleGround/attleGroundMgr.h"
+#include "BattleGround/BattleGround.h"
+#include "BattleGround/BattleGroundEY.h"
+#include "BattleGround/BattleGroundWS.h"
 #include "Language.h"
 #include "SocialMgr.h"
 #include "VMapFactory.h"

Dramacydal/murlocs_434

Conflicts:
	src/game/Group.cpp
	src/game/Map.cpp
	src/game/Spell.cpp
	src/game/SpellEffects.cpp
	src/game/Unit.cpp
	src/game/WorldSession.cpp
	src/game/debugcmds.cpp
	src/mangosd/WorldRunnable.cpp
Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

src/game/SpellEffects.cpp
@@ -42,11 +42,11 @@
 #include "Creature.h"
 #include "Totem.h"
 #include "CreatureAI.h"
-#include "BattleGroundMgr.h"
-#include "BattleGround.h"
-#include "BattleGroundEY.h"
-#include "BattleGroundWS.h"
-#include "BattleGroundSA.h"
+#include "BattleGround/BattleGroundMgr.h"
+#include "BattleGround/BattleGround.h"
+#include "BattleGround/BattleGroundEY.h"
+#include "BattleGround/BattleGroundWS.h"
+#include "BattleGround/BattleGroundSA.h"
 #include "OutdoorPvP/OutdoorPvPMgr.h"
 #include "Language.h"
 #include "SocialMgr.h"