df28bd5e579a79feb15e71ab971154028a8eb471
Yaki Khadafi <elsoldollo@gmail.com>
2012-10-18 00:24:55 +0300

    Add our sql changes (without content)
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

---
+++ sql_300m/format/char/char.sql
@@ -0,0 +1,241 @@
+CREATE TABLE IF NOT EXISTS `anticheat_log` (
+  `entry`          bigint(20) NOT NULL auto_increment,
+  `acctid`         int(11) NOT NULL,
+  `guid`           int(11) unsigned NOT NULL,
+  `playername`     varchar(32) NOT NULL,
+  `checktype`      mediumint(8) unsigned NOT NULL,
+  `alarm_time`     datetime NOT NULL,
+  `action1`        mediumint(8) NOT NULL default '0',
+  `action2`        mediumint(8) NOT NULL default '0',
+  `reason`         varchar(255) NOT NULL DEFAULT 'Unknown',
+  `Map`            smallint(5) NOT NULL default '-1',
+  `Pos`            varchar(255) NOT NULL default '0',
+  `Level`          mediumint(9) NOT NULL default '0',
+   PRIMARY KEY  (`entry`),
+   KEY `idx_Player` (`guid`)
+)  ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Anticheat log table';
+
+CREATE TABLE `armory_character_stats` (
+  `guid` int(11) NOT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY  (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='World of Warcraft Armory table';
+
+CREATE TABLE `armory_game_chart` (
+  `gameid` int(11) NOT NULL,
+  `teamid` int(11) NOT NULL,
+  `guid` int(11) NOT NULL,
+  `changeType` int(11) NOT NULL,
+  `ratingChange` int(11) NOT NULL,
+  `teamRating` int(11) NOT NULL,
+  `damageDone` int(11) NOT NULL,
+  `deaths` int(11) NOT NULL,
+  `healingDone` int(11) NOT NULL,
+  `damageTaken` int(11) NOT NULL,
+  `healingTaken` int(11) NOT NULL,
+  `killingBlows` int(11) NOT NULL,
+  `mapId` int(11) NOT NULL,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
+  `personalRating` int(11) NOT NULL,
+  `ip` text,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`gameid`,`teamid`,`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='WoWArmory Game Chart';
+
+ CREATE TABLE IF NOT EXISTS `auction_log` (
+	`guid` int(11) unsigned NOT NULL DEFAULT '0',
+	`seller` int(11) unsigned NOT NULL DEFAULT '0',
+	`item` int(11) unsigned NOT NULL DEFAULT '0',
+	`itemGuid` int(11) unsigned NOT NULL DEFAULT '0',
+	`money` int(11) NOT NULL DEFAULT '0',
+	`type` tinyint(3) NOT NULL DEFAULT '0',
+	`date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+ENGINE=InnoDb DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Auction log';
+
+CREATE TABLE `item_soulbound_trade_data` (
+  `itemGuid` int(16) unsigned NOT NULL DEFAULT '0',
+  `allowedPlayers` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`itemGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='BOP item trade cache';
+
+CREATE TABLE  `characters_visuals` (
+  `guid` int(11) unsigned NOT NULL,
+  `head` int(11) unsigned NOT NULL,
+  `shoulders` int(11) unsigned NOT NULL,
+  `chest` int(11) unsigned NOT NULL,
+  `waist` int(11) unsigned NOT NULL,
+  `legs` int(11) unsigned NOT NULL,
+  `feet` int(11) unsigned NOT NULL,
+  `wrists` int(11) unsigned NOT NULL,
+  `hands` int(11) unsigned NOT NULL,
+  `back` int(11) unsigned NOT NULL,
+  `main` int(11) unsigned NOT NULL,
+  `off` int(11) unsigned NOT NULL,
+  `ranged` int(11) unsigned NOT NULL,
+  `type` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Player extra visuality items';
+
+CREATE TABLE `character_account_log` (
+  `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier',
+  `oldacc` int(11) unsigned NOT NULL default '0' COMMENT 'Old Account Identifier',
+  `newacc` int(11) unsigned NOT NULL default '0' COMMENT 'New Account Identifier',
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Character transfer logs';
+
+CREATE TABLE `character_banned` (
+  `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Character guid',
+  `bandate` bigint(40) NOT NULL default '0',
+  `unbandate` bigint(40) NOT NULL default '0',
+  `bannedby` varchar(50) NOT NULL,
+  `banreason` varchar(255) NOT NULL,
+  `active` tinyint(4) NOT NULL default '1',
+  PRIMARY KEY  (`guid`,`bandate`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Character Ban List';
+
+CREATE TABLE IF NOT EXISTS `character_mmr` (
+  `guid` int(10) NOT NULL,
+  `slot` tinyint(3) NOT NULL,
+  `mmr` smallint(5) NOT NULL,
+  PRIMARY KEY (`guid`,`slot`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `character_pet`
+	CHANGE `Reactstate` `Reactstate` INT(10) UNSIGNED NOT NULL DEFAULT '0';
+
+CREATE TABLE `character_race_change` (
+  `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier',
+  `oldrace` int(11) unsigned NOT NULL default '0' COMMENT 'Old Race',
+  `newrace` int(11) unsigned NOT NULL default '0' COMMENT 'New Race',
+  `oldname` text,
+  `newname` text,
+  `date` timestamp NOT NULL default CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Character race/faction change logs';
+
+CREATE TABLE `character_battleground_random` (
+  `guid` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `saved_variables` ADD COLUMN `NextRandomBGResetTime` bigint(40) unsigned NOT NULL default '0' AFTER `NextWeeklyQuestResetTime`;
+
+CREATE TABLE `character_renames` (
+  `guid` int(11) NOT NULL,
+  `oldname` text,
+  `newname` text,
+  `date` int(11) NOT NULL,
+   PRIMARY KEY (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='';
+
+CREATE TABLE `console` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `done` timestamp NULL DEFAULT NULL,
+  `command` text,
+  `user` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `queuedGifts` (
+  `qIdx` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `charGuid` int(10) unsigned NOT NULL,
+  `itemId` int(10) unsigned NOT NULL,
+  `itemStackCnt` int(10) unsigned DEFAULT '1',
+  `itemGuid` int(10) unsigned DEFAULT NULL,
+  `sentFlag` int(10) unsigned DEFAULT '0',
+  `text` text,
+  `subj` varchar(255) DEFAULT '',
+  `gold` int(10) unsigned DEFAULT '0',
+  `timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`qIdx`)
+) ENGINE=InnoDB AUTO_INCREMENT=84071 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gm_played_time` (
+  `account` int(11) unsigned NOT NULL default '0' COMMENT 'Account Identifier',
+  `playedTime` int(11) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`account`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='GM played time tracking system';
+
+CREATE TABLE `gmlog_commands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `command` varchar(255) NOT NULL,
+  `account` int(10) unsigned NOT NULL,
+  `player` varchar(30) NOT NULL,
+  `posX` float NOT NULL,
+  `posY` float NOT NULL,
+  `posZ` float NOT NULL,
+  `selected_type` varchar(255) DEFAULT NULL,
+  `selected_guid` bigint(20) unsigned DEFAULT NULL,
+  `map` int(10) unsigned NOT NULL,
+  `ip` varchar(20) NOT NULL DEFAULT '0.0.0.0',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1351458 DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='GM logs';
+
+CREATE TABLE `item_instance_text` (
+`guid` INT UNSIGNED  NOT NULL,
+`text` LONGTEXT,
+ PRIMARY KEY ( `guid` )
+);
+
+INSERT INTO item_instance_text SELECT guid, text FROM item_instance as it WHERE it.text != '';
+ALTER TABLE item_instance DROP `text`;
+
+CREATE TABLE `item_refund` (
+  `item_guid` int(10) unsigned NOT NULL COMMENT 'Item GUID',
+  `player_guid` int(10) unsigned NOT NULL COMMENT 'Player GUID',
+  `paidMoney` int(10) unsigned NOT NULL DEFAULT '0',
+  `paidExtendedCost` smallint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`item_guid`,`player_guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Item Refund System';
+
+ALTER TABLE `group_member`
+	ADD COLUMN `roles` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Member roles bit mask' AFTER `subgroup`;
+ALTER TABLE `group_member`
+	CHANGE `assistant` `memberFlags` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Member flags bit mask';
+
+ALTER TABLE `groups` DROP `mainTank`,
+	DROP `mainAssistant`;
+
+ALTER TABLE `characters` ADD COLUMN `grantableLevels`  tinyint(3) unsigned NOT NULL default '0' AFTER `actionBars`;
+
+CREATE TABLE `account_forcepermission` (
+  `id` int(11) unsigned NOT NULL DEFAULT '0',
+  `security` int(3) unsigned DEFAULT '0',
+  `comment` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `character_feed_log` (
+  `guid` int(11) NOT NULL,
+  `type` smallint(1) NOT NULL,
+  `data` int(11) NOT NULL,
+  `date` int(11) DEFAULT NULL,
+  `counter` int(11) NOT NULL,
+  `difficulty` smallint(6) DEFAULT '-1',
+  PRIMARY KEY (`guid`,`type`,`data`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- optimizations
+ALTER TABLE `character_achievement_progress`
+	MODIFY COLUMN `date`  int(4) UNSIGNED NOT NULL AFTER `counter`;
+
+ALTER TABLE `character_queststatus` 
+	MODIFY COLUMN `status` TINYINT UNSIGNED NOT NULL,
+	MODIFY COLUMN `mobcount1` SMALLINT UNSIGNED NOT NULL,
+	MODIFY COLUMN `mobcount2` SMALLINT UNSIGNED NOT NULL AFTER `mobcount1`,
+	MODIFY COLUMN `mobcount3` SMALLINT UNSIGNED NOT NULL AFTER `mobcount2`,
+	MODIFY COLUMN `mobcount4` SMALLINT UNSIGNED NOT NULL AFTER `mobcount3`,
+	MODIFY COLUMN `itemcount1` SMALLINT UNSIGNED NOT NULL,
+	MODIFY COLUMN `itemcount2` SMALLINT UNSIGNED NOT NULL AFTER `itemcount1`,
+	MODIFY COLUMN `itemcount3` SMALLINT UNSIGNED NOT NULL AFTER `itemcount2`,
+	MODIFY COLUMN `itemcount4` SMALLINT UNSIGNED NOT NULL AFTER `itemcount3`,
+	MODIFY COLUMN `itemcount5` SMALLINT UNSIGNED NOT NULL AFTER `itemcount4`,
+	MODIFY COLUMN `itemcount6` SMALLINT UNSIGNED NOT NULL AFTER `itemcount5`,
+	MODIFY COLUMN `timer`  int(4) UNSIGNED NOT NULL AFTER `explored`;
+
+ALTER TABLE `character_reputation` CHANGE `faction` `faction` SMALLINT UNSIGNED NOT NULL;
+ALTER TABLE `character_reputation` CHANGE `flags` `flags` SMALLINT UNSIGNED NOT NULL;
+
+ALTER TABLE `pet_spell` CHANGE `active` `active` TINYINT UNSIGNED NOT NULL;
---
+++ sql_300m/format/world/world.sql
@@ -0,0 +1,147 @@
+﻿ALTER TABLE creature_template
+  ADD COLUMN `spell5` mediumint(8) unsigned NOT NULL default '0' AFTER `spell4`,
+  ADD COLUMN `spell6` mediumint(8) unsigned NOT NULL default '0' AFTER `spell5`,
+  ADD COLUMN `spell7` mediumint(8) unsigned NOT NULL default '0' AFTER `spell6`,
+  ADD COLUMN `spell8` mediumint(8) unsigned NOT NULL default '0' AFTER `spell7`,
+  ADD COLUMN `powerType` mediumint(8) unsigned NOT NULL default '0' AFTER `maxhealth`,
+  CHANGE COLUMN unk16 health_mod float,
+  CHANGE COLUMN unk17 power_mod float;
+  
+CREATE TABLE `autobroadcast` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `creature_transport` (
+ `guid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `entry` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ `map` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ `position_x` float DEFAULT '0',
+ `position_y` float DEFAULT '0',
+ `position_z` float DEFAULT '0',
+ `orientation` float DEFAULT '0',
+ `emote` mediumint(8) unsigned DEFAULT '0',
+ PRIMARY KEY (`guid`,`map`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Creature on transports';
+
+CREATE TABLE `custom_areaflags` (
+ `entry` mediumint(8) unsigned NOT NULL DEFAULT '0',
+ `flags` int(10) unsigned NOT NULL DEFAULT '0',
+ `comment` char(100) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`entry`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Custom area flags table';
+
+CREATE TABLE `custom_worldsafelocs` (
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+ `map` int(10) unsigned,
+ `x` float DEFAULT '0',
+ `y` float DEFAULT '0',
+ `z` float DEFAULT '0',
+ `team` int(10) unsigned,
+ `comment` text,
+ PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Custom world safe locs';
+
+ALTER TABLE `areatrigger_teleport` CHANGE `required_quest_done` `required_quest_done_A` int(11) UNSIGNED NOT NULL DEFAULT "0"
+COMMENT "Alliance quest";
+
+ALTER TABLE `areatrigger_teleport` CHANGE `required_quest_done_heroic` `required_quest_done_heroic_A` int(11) UNSIGNED NOT NULL DEFAULT "0"
+COMMENT "Alliance heroic quest";
+
+ALTER TABLE `areatrigger_teleport` ADD COLUMN
+`required_quest_done_H` int(11) UNSIGNED NOT NULL DEFAULT "0" COMMENT "Horde quest"
+AFTER `required_quest_done_A`;
+
+ALTER TABLE `areatrigger_teleport` ADD COLUMN
+`required_quest_done_heroic_H` int(11) UNSIGNED NOT NULL DEFAULT "0" COMMENT "Horde heroic quest"
+AFTER `required_quest_done_heroic_A`;
+
+ALTER TABLE `areatrigger_teleport` ADD COLUMN
+`minGS` int(11) UNSIGNED NOT NULL DEFAULT "0" COMMENT "Min player gear score";
+
+ALTER TABLE `areatrigger_teleport` ADD COLUMN
+`maxGS` int(11) UNSIGNED NOT NULL DEFAULT "0" COMMENT "Max player gear score";
+
+UPDATE `areatrigger_teleport` SET `required_quest_done_H` = `required_quest_done_A`
+WHERE `required_quest_done_A` > 0;
+
+UPDATE `areatrigger_teleport` SET `required_quest_done_heroic_H` = `required_quest_done_heroic_A`
+WHERE `required_quest_done_heroic_A` > 0;
+
+-- LFG dungeon reward structure from TC
+
+CREATE TABLE `lfg_dungeon_rewards` (
+  `dungeonId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Dungeon entry from dbc',
+  `maxLevel` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'Max level at which this reward is rewarded',
+  `firstQuestId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Quest id with rewards for first dungeon this day',
+  `firstMoneyVar` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Money multiplier for completing the dungeon first time in a day with less players than max',
+  `firstXPVar` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Experience multiplier for completing the dungeon first time in a day with less players than max',
+  `otherQuestId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Quest id with rewards for Nth dungeon this day',
+  `otherMoneyVar` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Money multiplier for completing the dungeon Nth time in a day with less players than max',
+  `otherXPVar` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Experience multiplier for completing the dungeon Nth time in a day with less players than max',
+  PRIMARY KEY (`dungeonId`,`maxLevel`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `player_factionchange_spells` (
+    `alliance_id` int(8) NOT NULL,
+    `commentA` varchar(255) DEFAULT NULL,
+    `horde_id` int(8) NOT NULL,
+    `commentH` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`alliance_id`, `horde_id`)
+) DEFAULT CHARSET=UTF8;
+
+DROP TABLE IF EXISTS `player_factionchange_reputations`;
+CREATE TABLE `player_factionchange_reputations` (
+    `alliance_id` int(8) NOT NULL,
+    `commentA` varchar(255) DEFAULT NULL,
+    `horde_id` int(8) NOT NULL,
+    `commentH` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`alliance_id`, `horde_id`)
+) DEFAULT CHARSET=UTF8;
+
+DROP TABLE IF EXISTS `player_factionchange_achievements`;
+CREATE TABLE `player_factionchange_achievements` (
+    `alliance_id` int(8) NOT NULL,
+    `horde_id` int(8) NOT NULL,
+    `commentA` varchar(255) NOT NULL,
+    `commentH` varchar(255) NOT NULL,
+    PRIMARY KEY (`alliance_id`,`horde_id`)
+) DEFAULT CHARSET=UTF8;
+
+DROP TABLE IF EXISTS `player_factionchange_items`;
+CREATE TABLE `player_factionchange_items` (
+    `alliance_id` int(8) NOT NULL,
+    `commentA` varchar(255) DEFAULT NULL,
+    `horde_id` int(8) NOT NULL,
+    `commentH` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`alliance_id`,`horde_id`)
+) DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `player_factionchange_titles` (
+    `alliance_id` int(8) NOT NULL,
+    `commentA` varchar(255) DEFAULT NULL,
+    `horde_id` int(8) NOT NULL,
+    `commentH` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`alliance_id`, `horde_id`)
+) DEFAULT CHARSET=UTF8;
+
+ALTER TABLE `creature_onkill_reputation` 
+    ADD COLUMN `ChampioningAura` int(11) unsigned NOT NULL default '0' AFTER `TeamDependent`;
+	
+CREATE TABLE `pet_levelstats` (
+  `creature_entry` mediumint(8) unsigned NOT NULL,
+  `level` tinyint(3) unsigned NOT NULL COMMENT 'Level',
+  `hp` smallint(5) unsigned NOT NULL COMMENT 'Health',
+  `mana` smallint(5) unsigned NOT NULL COMMENT 'Mana',
+  `armor` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Armor',
+  `mindmg` mediumint(11) NOT NULL DEFAULT '0' COMMENT 'Min base damage',
+  `maxdmg` mediumint(11) NOT NULL DEFAULT '0' COMMENT 'Max base damage',
+  `attackpower` mediumint(11) NOT NULL DEFAULT '0' COMMENT 'Attack power',
+  `str` smallint(5) unsigned NOT NULL COMMENT 'Strength',
+  `agi` smallint(5) unsigned NOT NULL COMMENT 'Agility',
+  `sta` smallint(5) unsigned NOT NULL COMMENT 'Stamina',
+  `inte` smallint(5) unsigned NOT NULL  COMMENT 'Intellect',
+  `spi` smallint(5) unsigned NOT NULL  COMMENT 'Spirit',
+  PRIMARY KEY (`creature_entry`,`level`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 PACK_KEYS=0 COMMENT='Stores pet levels stats.';
