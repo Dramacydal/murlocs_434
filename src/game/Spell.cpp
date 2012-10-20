f6edbbbd451b17fccf8985589cbae60c8ac745d4
Yaki Khadafi <elsoldollo@gmail.com>
2012-10-20 21:16:31 +0300

    Some tweaks to worgen transformations
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -3839,13 +3839,14 @@ void Spell::cast(bool skipCheck)
             else if (m_spellInfo->Id == 68992)              // Darkflight
             {
                 AddPrecastSpell(96223);                     // Run Speed Marker
-                AddPrecastSpell(97709);                     // Altered Form
+                if (m_caster->HasWorgenForm())
+                    AddPrecastSpell(97709);                 // Altered Form
             }
             else if (m_spellInfo->Id == 68996)              // Two Forms
             {
                 if (m_caster->IsInWorgenForm())
                     m_caster->RemoveSpellsCausingAura(SPELL_AURA_WORGEN_TRANSFORM);
-                else
+                else if (m_caster->HasWorgenForm())
                     AddPrecastSpell(97709);                 // Altered Form
             }
             // Chaos Bane strength buff
@@ -7043,6 +7044,12 @@ SpellCastResult Spell::CheckCast(bool strict)
 
                 break;
             }*/
+            case SPELL_AURA_WORGEN_TRANSFORM:
+            {
+                if (!m_caster->HasWorgenForm())
+                    return m_IsTriggeredSpell ? SPELL_FAILED_DONT_REPORT : SPELL_FAILED_CANT_DO_THAT_RIGHT_NOW;
+                break;
+            }
             default:
                 break;
         }
