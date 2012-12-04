5824581ada831377e4675b970f9d1b039bd08102
Xfurry <xfurry@scriptdev2.com>
2012-12-04 19:20:28 +0100

    [12328] Add positive exception for spell 18153
    (based on commit [12244] - eabba370d31ffcb8058019b67b8b837ca1dabc33)
    
    Conflicts:
    	src/game/SpellMgr.cpp
    	src/shared/revision_nr.h
    
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -1157,6 +1157,7 @@ bool IsPositiveEffect(SpellEntry const *spellproto, SpellEffectIndex effIndex)
                     return false;
                 case 48021:                                 // support for quest 12173
                     return true;
+                case 18153:                                 // Kodo Kombobulator
                 case 49634:                                 // Sergeant's Flare
                 case 54530:                                 // Opening
                 case 62105:                                 // To'kini's Blowgun
