4cadf84893c672409cb21d955243fcd0eef7d67f
Yaki Khadafi <elsoldollo@gmail.com>
2012-12-14 17:28:36 +0300

    Logging to Spell::DoSummonWild
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -7239,7 +7239,13 @@ void Spell::DoSummonWild(SpellEffectEntry const * effect, uint32 forceFaction)
             // Ghoul emote
             if (summon->GetEntry() == 24207 || summon->GetEntry() == 26125 || summon->GetEntry() == 28528)
                 summon->HandleEmote(EMOTE_ONESHOT_EMERGE);
+
+            DEBUG_LOG("DoSummonWild: summoned npc %u from spell %u at %f %f %f map %u summonType %u duration %i",
+                creature_entry, m_spellInfo->Id, px, py, pz, m_caster->GetMapId(), summonType, m_duration);
         }
+        else
+            sLog.outError("DoSummonWild: failed to summon npc %u from spell %u at %f %f %f map %u summonType %u duration %i",
+                creature_entry, m_spellInfo->Id, px, py, pz, m_caster->GetMapId(), summonType, m_duration);
     }
 }
 
