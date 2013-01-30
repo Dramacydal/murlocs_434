c3d2a17b5781c60ed302a9dcde60af4fe4665c0e
Yaki Khadafi <elsoldollo@gmail.com>
2013-01-30 16:33:53 +0300

    Glyph of Gouge
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -6192,7 +6192,8 @@ SpellCastResult Spell::CheckCast(bool strict)
         }
 
         // Target must be facing you.
-        if ((m_spellInfo->Attributes == (SPELL_ATTR_UNK4 | SPELL_ATTR_NOT_SHAPESHIFT | SPELL_ATTR_UNK18 | SPELL_ATTR_STOP_ATTACK_TARGET)) && !target->HasInArc(M_PI_F, m_caster))
+        if ((m_spellInfo->Attributes == (SPELL_ATTR_UNK4 | SPELL_ATTR_NOT_SHAPESHIFT | SPELL_ATTR_UNK18 | SPELL_ATTR_STOP_ATTACK_TARGET)) && !target->HasInArc(M_PI_F, m_caster) &&
+            (m_spellInfo->Id != 1776 || !m_caster->HasAura(56809))) // Glyph of Gouge allows cast Gouge when not in front
         {
             SendInterrupted(2);
             return SPELL_FAILED_NOT_INFRONT;
