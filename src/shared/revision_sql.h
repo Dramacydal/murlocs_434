0121ddd8c486303b4834120e2d2024df7bed9e03
Cyberium <cyberium@techemail.com>
2013-02-15 00:21:23 +0200

    [12453] More correct help string for .wp show command.
    Thanks @alexluana for pointing.
    
    (based on commit [12344] - 63f4f24)
    
    Conflicts:
    	sql/mangos.sql
    	src/shared/revision_nr.h
    	src/shared/revision_sql.h
    
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_CHARACTERS "required_12300_02_characters_mail"
- #define REVISION_DB_MANGOS "required_12378_14_mangos_gossip_menu_option"
+ #define REVISION_DB_CHARACTERS "required_12447_02_characters_calendar_invites"
+ #define REVISION_DB_MANGOS "required_12453_01_mangos_command"
  #define REVISION_DB_REALMD "required_12112_01_realmd_account_access"
 #endif // __REVISION_SQL_H__
