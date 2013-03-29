eb4e4fab58e782ad5fea49593c2eea2f72b1f95e
Yaki Khadafi <elsoldollo@gmail.com>
2013-03-29 13:09:01 +0300

    test
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/NPCHandler.cpp
+++ src/game/NPCHandler.cpp
@@ -613,8 +613,7 @@ void WorldSession::SendStablePet( ObjectGuid guid )
             data << uint32(fields[2].GetUInt32());          // creature entry
             data << uint32(fields[3].GetUInt32());          // level
             data << fields[4].GetString();                  // name
-            //data << uint8(fields[5].GetUInt32() < PET_SAVE_FIRST_STABLE_SLOT ? 1 : 3);  // 1 = current, 2/3 = in stable (any from 4,5,... create problems with proper show)
-            data << uint8(0);  // 1 = current, 2/3 = in stable (any from 4,5,... create problems with proper show)
+            data << uint8(fields[5].GetUInt32() < PET_SAVE_FIRST_STABLE_SLOT ? 1 : 3);  // 1 = current, 2/3 = in stable (any from 4,5,... create problems with proper show)
 
             ++num;
         }while( result->NextRow() );
@@ -624,6 +623,8 @@ void WorldSession::SendStablePet( ObjectGuid guid )
 
     data.put<uint8>(wpos, num);                             // set real data to placeholder
     SendPacket(&data);
+
+    SendStableResult(STABLE_SUCCESS_STABLE);
 }
 
 void WorldSession::SendStableResult(uint8 res)
