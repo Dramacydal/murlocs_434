f98f77b3a906f448157facfedb2415525f5a0ab3
Yaki Khadafi <elsoldollo@gmail.com>
2013-04-15 14:26:51 +0300

    debug
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -282,7 +282,10 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
     data->WriteBit(updateFlags & UPDATEFLAG_SELF);
     data->WriteBit(updateFlags & UPDATEFLAG_VEHICLE);
     data->WriteBit(updateFlags & UPDATEFLAG_LIVING);
-    data->WriteBits(0, 24);                                     // Byte Counter
+    if (GetEntry() == 207547)
+        data->WriteBits(2, 24);
+    else
+        data->WriteBits(0, 24);                                     // Byte Counter
     data->WriteBit(false);
     data->WriteBit(updateFlags & UPDATEFLAG_POSITION);                // flags & UPDATEFLAG_HAS_POSITION Game Object Position
     data->WriteBit(updateFlags & UPDATEFLAG_HAS_POSITION);            // Stationary Position
@@ -405,6 +408,12 @@ void Object::BuildMovementUpdate(ByteBuffer * data, uint16 updateFlags) const
     }
 
     data->FlushBits();
+
+    if (GetEntry() == 207547)
+    {
+        *data << int32(10000);
+        *data << int32(16667);
+    }
 
     if (updateFlags & UPDATEFLAG_LIVING)
     {
