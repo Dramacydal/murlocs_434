mangosfour/server_old_0.17

[12320] Implement UNIT_STAT_RUNNING to mark default running behaviour. Clone #17
(based on commit [12220] - 29db8ae0878b62b7747d2cf491fbef830bd6e81f)

src/game/SpellEffects.cpp
@@ -5356,7 +5356,7 @@ bool Spell::DoSummonPossessed(CreatureSummonPositions& list, SummonPropertiesEnt
 
     spawnCreature->SetLevel(level);
 
-    spawnCreature->SetWalk(m_caster->IsWalking());
+    spawnCreature->SetWalk(m_caster->IsWalking(), true);
     // TODO: Set Fly (ie glyph dependend)
 
     // Internal changes

Dramacydal/murlocs_434

Conflicts:
	src/game/Creature.cpp
	src/game/RandomMovementGenerator.cpp
	src/game/SpellEffects.cpp
	src/game/Unit.h
	src/game/Vehicle.cpp
	src/game/WaypointMovementGenerator.cpp
	src/shared/revision_nr.h
Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

src/game/SpellEffects.cpp
@@ -7350,6 +7350,9 @@ void Spell::DoSummonPossessed(SpellEffectEntry const * effect)
         if (CharmInfo* charmInfo = summon->InitCharmInfo(summon))
             charmInfo->InitPossessCreateSpells();
         player->PossessSpellInitialize();
+
+        summon->SetWalk(m_caster->IsWalking(), true);
+        // TODO: Set Fly (ie glyph dependend)
     }
 
     // Notify Summoner