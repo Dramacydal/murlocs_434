c47cc7090ea4597960f22b6819d435d7f6fe8385
Yaki Khadafi <elsoldollo@gmail.com>
2013-04-27 00:40:49 +0300

    test transport change
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -142,7 +142,10 @@ bool GameObject::Create(uint32 guidlow, uint32 name_id, Map *map, uint32 phaseMa
         return false;
     }
 
-    Object::_Create(guidlow, goinfo->id, goinfo->type == GAMEOBJECT_TYPE_TRANSPORT ? HIGHGUID_MO_TRANSPORT : HIGHGUID_GAMEOBJECT);
+    if (goinfo->type == GAMEOBJECT_TYPE_TRANSPORT)
+        Object::_Create(guidlow, 0, HIGHGUID_MO_TRANSPORT);
+    else
+        Object::_Create(guidlow, goinfo->id, HIGHGUID_GAMEOBJECT);
 
     m_goInfo = goinfo;
 
