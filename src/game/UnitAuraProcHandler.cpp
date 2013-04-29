08f41d49f0edf1e3743d0344d220b172527b15cc
Yaki Khadafi <elsoldollo@gmail.com>
2013-04-29 10:38:47 +0300

    Item - Warrior T13 Arms and Fury 4P Bonus (Colossus Smash)
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/UnitAuraProcHandler.cpp
+++ src/game/UnitAuraProcHandler.cpp
@@ -4593,6 +4593,17 @@ SpellAuraProcResult Unit::HandleProcTriggerSpellAuraProc(Unit *pVictim, uint32 d
                     return SPELL_AURA_PROC_FAILED;
                 break;
             }
+            else if (auraSpellInfo->Id == 105907)           // Item - Warrior T13 Arms and Fury 4P Bonus (Colossus Smash)
+            {
+                if (!procSpell)
+                    return SPELL_AURA_PROC_FAILED;
+
+                // Bloodthirst has 2 times lower chance to proc
+                if (procSpell->Id == 23881)
+                    if (!roll_chance_i(50))
+                        return SPELL_AURA_PROC_FAILED;
+                break;
+            }
             break;
         case SPELLFAMILY_WARLOCK:
         {
