ac1d5873edd0df0a84bc667c3a5af7618d60ca08
Yaki Khadafi <elsoldollo@gmail.com>
2013-05-03 23:44:11 +0300

    correct fishing chances
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6098,7 +6098,15 @@ bool Player::UpdateFishingSkill()
 
     uint32 SkillValue = GetPureSkillValue(SKILL_FISHING);
 
-    int32 chance = SkillValue < 75 ? 100 : 2500/(SkillValue-50);
+    int32 chance = 100;
+    if (SkillValue >= 450)
+        chance = 18.18f;
+    else if (SkillValue >= 300)
+        chance = 27.78f;
+    else if (SkillValue >= 200)
+        chance = 52.63f;
+    else if (SkillValue >= 100)
+        chance = 62.5f;
 
     uint32 gathering_skill_gain = sWorld.getConfig(CONFIG_UINT32_SKILL_GAIN_GATHERING);
 
